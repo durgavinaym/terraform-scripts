025efe8d7d2fe7c8ddb52f6f35a0a27404dcd8b2
durgavinaym <90096333+durgavinaym@users.noreply.github.com>
2022-09-16 20:15:30 +0530

    Update jenkins-install.sh

--- Jenkins-infra/jenkins-install.sh
+++ Jenkins-infra/jenkins-install.sh
@@ -6,7 +6,7 @@
   # (you can upadte the specific version from the link https://www.jenkins.io/doc/book/installing/linux/)
   curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo tee \
   /usr/share/keyrings/jenkins-keyring.asc > /dev/null
-  echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
   https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
   /etc/apt/sources.list.d/jenkins.list > /dev/null
   sudo apt-get -y update
